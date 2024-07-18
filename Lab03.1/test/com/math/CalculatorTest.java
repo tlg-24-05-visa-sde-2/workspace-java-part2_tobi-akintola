94a523028876763d60ab49b504ff044809cf3371
Oluwatobi Akintola <oluwatobia@tlgcohort.com>
2024-07-18 13:25:53 -0500

    added lifecycle methods

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,26 +9,50 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    //business objects under test - called the fixture
+    private Calculator calc;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("beforeClass");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("afterClass");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
 
     @Test
     public void testIsEven() {
-        Calculator calc = new Calculator();
+        System.out.println("testIsEven");
         assertTrue(calc.isEven(10));
         assertFalse(calc.isEven(11));
     }
 
     @Test
     public void testDivide() {
-        Calculator calc = new Calculator();
+        System.out.println("testDivide");
         assertEquals(2.5, calc.divide(5, 2), .001); //expected, actual, delta(.001)
     }
 
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
+        System.out.println("testAdd");
         assertEquals(5, calc.add(1, 4));  // expected, actual
     }
 }
