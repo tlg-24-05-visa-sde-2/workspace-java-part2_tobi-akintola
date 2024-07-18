7d776e2c521bb632b8b5b0d4c138764348fe4553
Oluwatobi Akintola <oluwatobia@tlgcohort.com>
2024-07-18 11:27:30 -0500

    test classes setup

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -14,6 +14,19 @@ import org.junit.Test;
 public class CalculatorTest {
 
     @Test
+    public void testIsEven() {
+        Calculator calc = new Calculator();
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(11));
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calc = new Calculator();
+        assertEquals(2.5, calc.divide(5, 2), .001); //expected, actual, delta(.001)
+    }
+
+    @Test
     public void testAdd() {
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(1, 4));  // expected, actual
